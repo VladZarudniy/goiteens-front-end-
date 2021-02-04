2fa19e9291bf8bce68b5ec721cede6cc7c3a52c4
Vlad Zarudniy <nflary_@MacBook-Pro-Vlad.local>
2021-02-04 19:40:41 +0200

    JS1

---
+++ JavaScript/pr1/js/script.js
@@ -0,0 +1,39 @@
+let str = "Привіт"
+let num = 123
+let flag = true
+let txt = "true"
+
+
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt)
+
+//second part of task
+console.log("")
+console.log("second part of task")
+console.log("")
+
+let nick1 = "first let"
+let  s = 2
+let  ok = true
+let  fn = 16
+let  nick2 = "last let"
+const  nick3 ="first const"
+const  a = 90
+const  b1 = false
+const  nick4 = "okey hokey"
+const  x = 53434524
+//
+console.log("nick1 =" ,nick1 , ", type is" , typeof nick1)
+console.log("s = " ,s, ", type is" , typeof s)
+console.log("ok = " ,ok, ", type is" , typeof ok)
+console.log("fn = " ,fn, ", type is" , typeof fn)
+console.log("nick2 = " ,nick2, ", type is" , typeof nick2)
+//
+console.log("nick3 = " ,nick3, ", type is" , typeof nick3)
+console.log("a = " ,a, ", type is" , typeof a)
+console.log("b1 = " ,b1, ", type is" , typeof b1)
+console.log("nick4 = " ,nick4, ", type is" , typeof nick4)
+console.log("x = " , x , ", type is" , typeof x)
