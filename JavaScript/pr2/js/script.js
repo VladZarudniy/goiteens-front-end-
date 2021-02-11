0ca0394d5a152e6554123e17734c2f625297651d
Vlad Zarudniy <nflary_@MacBook-Pro-Vlad.local>
2021-02-11 17:45:50 +0200

    js2

---
+++ JavaScript/pr2/js/script.js
@@ -0,0 +1,76 @@
+let name = prompt("write your name" , "here write your name pls")
+
+console.log(name)
+
+let my = 12; 
+let fr = 13;
+
+//console.log(my + fr)
+
+alert(my + fr)
+
+let a = 12
+let b = 214
+
+console.log(a*b)
+
+console.log('')
+console.log('')
+
+
+let a1 = 5+3
+let a2 = 5-3
+let a3 = 5*3
+let a4 = 5/3
+console.log("a1 =",a1)
+console.log("a2 =",a2)
+console.log("a3 =",a3)
+console.log("a4 =",a4)
+
+console.log()
+console.log()
+console.log()
+console.log()
+
+let a6 = 5 % 3
+let a7 = 3 % 5
+let a8 = 5 + '3'
+let a9 = '5' - 3
+let a10 = 75 + 'kg'
+console.log("a6 =",a6)
+console.log("a7 =",a7)
+console.log("a8 =",a8)
+console.log("a9 =",a9)
+console.log("a10 =",a10)
+
+console.log("")
+console.log("")
+console.log("")
+console.log("")
+
+let height = prompt("write height of the box in cm", "23")
+console.log(height)
+let width = prompt("write width of the box in cm", "10")
+console.log(width)
+alert(height*width,"is your box")
+
+console.log(height*width ,'cm is your box')
+console.log("")
+console.log("")
+
+
+let heightC = prompt('write pls width of your cilindr in m', '10')
+let radius = prompt('write pls radius of your cilindr in m','4')
+let p = 3.141592
+let cilindr = p*(radius**2)*heightC
+alert(cilindr,"is V of your cilindr")
+console.log(cilindr)
+
+console.log("")
+console.log("")
+
+let n = prompt('write pls length of your n','3')
+let m = prompt('write pls length of your m', '4')
+let c = Math.sqrt(n**2+m**2)
+alert(c)
+console.log(c)
