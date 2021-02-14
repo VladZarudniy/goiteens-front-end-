0e3da4991be42ed16ee80b9f5815afd233b2d55e
Vlad Zarudniy <nflary_@MacBook-Pro-Vlad.local>
2021-02-14 19:25:03 +0200

    pr2-2

--- JavaScript/pr2/js/script.js
+++ JavaScript/pr2/js/script.js
@@ -1,76 +1,76 @@
-let name = prompt("write your name" , "here write your name pls")
+let name = prompt("write your name" , "here write your name pls");
 
-console.log(name)
+console.log(name);
 
 let my = 12; 
 let fr = 13;
 
 //console.log(my + fr)
 
-alert(my + fr)
+alert(my + fr);
 
-let a = 12
-let b = 214
+let a = 12;
+let b = 214;
 
-console.log(a*b)
+console.log(a*b);
 
-console.log('')
-console.log('')
-
-
-let a1 = 5+3
-let a2 = 5-3
-let a3 = 5*3
-let a4 = 5/3
-console.log("a1 =",a1)
-console.log("a2 =",a2)
-console.log("a3 =",a3)
-console.log("a4 =",a4)
-
-console.log()
-console.log()
-console.log()
-console.log()
-
-let a6 = 5 % 3
-let a7 = 3 % 5
-let a8 = 5 + '3'
-let a9 = '5' - 3
-let a10 = 75 + 'kg'
-alert(a6)
-alert(a7)
-alert(a8)
-alert(a9)
-alert(a10)
-
-console.log("")
-console.log("")
-console.log("")
-console.log("")
-
-let height = prompt("write height of the box in cm", "23")
-console.log(height)
-let width = prompt("write width of the box in cm", "10")
-console.log(width)
-alert(height*width)
-
-console.log(height*width ,'cm is your box')
-console.log("")
-console.log("")
+console.log('');
+console.log('');
 
 
-let heightC = prompt('write pls width of your cilindr in m', '10')
-let radius = prompt('write pls radius of your cilindr in m','4')
-let p = 3.141592
-let cilindr = p*(radius**2)*heightC
-alert(cilindr,"is V of your cilindr")
-console.log(cilindr)
+const a1 = 5+3;
+const a2 = 5-3;
+const a3 = 5*3;
+const a4 = 5/3;
+console.log("a1 =",a1);
+console.log("a2 =",a2);
+console.log("a3 =",a3);
+console.log("a4 =",a4);
 
-console.log("")
-console.log("")
+console.log();
+console.log();
+console.log();
+console.log();
 
-let n = prompt('write pls length of your n','3')
-let m = prompt('write pls length of your m', '4')
-let c = Math.sqrt(n**2+m**2)
-alert(c)
-console.log(c)
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'kg';
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+console.log("");
+console.log("");
+console.log("");
+console.log("");
+
+let height = 23;
+console.log(height);
+let width = 10;
+console.log(width);
+alert(height*width);
+
+console.log(height*width ,'cm is your box');
+console.log("");
+console.log("");
+
+
+let heightC = 10;
+let dc = 4;
+let p = 3.14;
+let v = p*((dc/2)**2)*heightC; // 3,14*4
+alert(v,"is V of your cilindr");
+console.log(v);
+
+console.log("");
+console.log("");
+
+let n = 3;
+let m = 4;
+let c = Math.sqrt(n**2+m**2);
+alert(c);
+console.log(c);
